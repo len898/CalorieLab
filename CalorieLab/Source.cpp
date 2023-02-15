128a66e249b66381b42e0cfa435fd03ffb4a8c5c
lennart@familie-richter.de <lennart@familie-richter.de>
2023-02-14 16:26:16 -0800

    Finished Calorie Calculator

--- CalorieLab/Source.cpp
+++ CalorieLab/Source.cpp
@@ -5,14 +5,19 @@ int main() {
 	
 	int age, heartRate, minutes, weight;
 
-	std::cout << "Enter your age: ";
+	//std::cout << "Enter your age: ";
 	std::cin >> age;
-	std::cout << "Enter heartRate during workout: ";
-	std::cin >> heartRate;
-	std::cout << "Enter the length of your workout in minutes: ";
-	std::cin >> minutes;
-	std::cout << "Enter your weight in pounds: ";
+	//std::cout << "Enter your weight in pounds: ";
 	std::cin >> weight;
+	//std::cout << "Enter heartRate during workout: ";
+	std::cin >> heartRate;
+	//std::cout << "Enter the length of your workout in minutes: ";
+	std::cin >> minutes;
+	
+
+	float calories = ((age * 0.2757) + (weight * 0.03295) + (heartRate * 1.0781) - 75.4991) * minutes / 8.368;
+
+	std::cout << std::fixed << std::setprecision(2) << "Calories: " << calories << " calories" << std::endl;
 
 	return 0;
 }
