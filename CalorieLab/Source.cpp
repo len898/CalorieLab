3975140761b8d0f114f28b60fccbc5bc7a004a8e
lennart@familie-richter.de <lennart@familie-richter.de>
2023-02-14 16:13:15 -0800

    Initialized Variables

---
+++ CalorieLab/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+int main() {
+	
+	int age, heartRate, minutes, weight;
+
+	std::cout << "Enter your age: ";
+	std::cin >> age;
+	std::cout << "Enter heartRate during workout: ";
+	std::cin >> heartRate;
+	std::cout << "Enter the length of your workout in minutes: ";
+	std::cin >> minutes;
+	std::cout << "Enter your weight in pounds: ";
+	std::cin >> weight;
+
+	return 0;
+}
